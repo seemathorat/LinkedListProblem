be6ed9723b3265f8b3f904c06ce6c22ca69aec29
seema thorat <seemagade97@gmail.com>
2021-03-15 00:10:17 +0530

    Add Element in Last Position

--- main/java/com/LinkedList.java
+++ main/java/com/LinkedList.java
@@ -43,6 +43,19 @@ public class LinkedList<T> {
             head.Next = temp;
         }
     }
+    public void addAtEnd(T data) {
+        Node<T> newNode = new Node<T>(data);
+        if(head == null) {
+
+            head = newNode;
+            tail = newNode;
+        }
+        else {
+            tail.Next = newNode;
+            tail = newNode;
+        }
+    }
+
 
 
     public void displayList() {
@@ -74,8 +87,9 @@ public class LinkedList<T> {
 
         list.addNode(70);
 
-        list.addNode(30);
-        list.addNode(56);
+        list.addAtEnd(30);
+        list.addAtEnd(56);
+        list.addAtEnd(70);
         list.displayList();
 
 
